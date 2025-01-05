d5a7c4917579b35d9a5884e8170f7a30b921440a
Ayush4998 <git@github.com:Ayush4998/abc.git>
2025-01-06 01:27:59 +0530

    Deployement

--- app.py
+++ app.py
@@ -52,7 +52,8 @@ def get_decision():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
 
 """  from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
